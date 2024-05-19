ef32db0616587beb7fb5bc22de3b4f7d2114d8dc
diegoengelhard <00356119@uca.edu.sv>
2024-05-18 22:12:23 -0600

    update: main app file

--- todo-app/src/main/java/com/engelhard/todoapp/TodoAppApplication.java
+++ todo-app/src/main/java/com/engelhard/todoapp/TodoAppApplication.java
@@ -3,11 +3,24 @@ package com.engelhard.todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		try {
+			System.out.println("Application started");
+			// TODO: Add listener for DB connection
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 
 }
